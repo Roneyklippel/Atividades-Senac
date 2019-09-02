e389707c5d3f441e203a558e08c22ac7308e2ba8
roneyklippel <sala302b@ceines.com.br>
2019-09-02 20:06:22 -0300

    Atividade1.Java

--- src/atividade/Atividade.java
+++ src/atividade/Atividade.java
@@ -1,24 +1,20 @@
-
 package atividade;
-
 
 public class Atividade {
 
     public static void main(String[] args) {
-       int cateto1 = 10;
-       int cateto2 = 20;
-       
-       double hipotenusa = teoremaPitaguras(cateto1,cateto2);
-        System.out.println("hipotenusa");
+        int cateto1 = 10;
+        int cateto2 = 20;
+
+        double hipotenusa = teoremaPitaguras(cateto1, cateto2);
+        System.out.println(hipotenusa);
+
     }
 
     private static double teoremaPitaguras(int cateto1, int cateto2) {
-       double hipotenusa = (cateto1*cateto1)+(cateto2*cateto2);
-       return Math.sqrt(hipotenusa);
-       
-        
-        
+        double hipotenusa = (cateto1 * cateto1) + (cateto2 * cateto2);
+        return Math.sqrt(hipotenusa);
+
     }
-    
-    
+
 }
